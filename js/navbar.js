bb7a6f05ee3ef34f65a8f627188343543b138206
Patrick Pacunana <pacunana.pat@gmail.com>
2020-03-03 23:49:53 -0800

    Added Navbar to Pages
    Transfered the updated Navbar to the other pages on the website

--- js/navbar.js
+++ js/navbar.js
@@ -3,16 +3,16 @@
 // console.log('JavaScript is running.');
 // window.onscroll = function() {myFunction()};
       
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
 // var coll = document.getElementsByClassName("collapsible");
 // var i;
